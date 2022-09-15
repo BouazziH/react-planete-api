beb92e3c99d3cc8b054805a511ba69f598d6f078
BouazziH <bouazzicdi@gmail.com>
2022-09-15 16:24:37 +0200

    Projet react pour afficher les donnees d'une api externe

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import './App.css';
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch(
+"http://localhost:7500/planetes")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  {
+                items.map((item) => ( 
+                  <div className="container">
+                    
+                <ul key = { item.id } >
+                
+                   <li> <img src={item.url}/></li>
+                    <li>Name: { item.name }</li>
+                    <li> date: { item.created_at } </li>
+                   
+                    </ul>
+                    </div>
+                ))
+            }
+        </div>
+        
+    );
+  
+}
+}
+   
+export default App;
